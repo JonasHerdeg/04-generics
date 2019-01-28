b360dc6ec0506b2979b544e9e76b1ac7525b106c
Jonas Herdeg <jonas.herdeg@orca-software.com>
2019-01-28 15:56:30 +0100

    test

--- src/main/java/de/thro/inf/prg3/a04/collections/SimpleList.java
+++ src/main/java/de/thro/inf/prg3/a04/collections/SimpleList.java
@@ -1,10 +1,14 @@
 package de.thro.inf.prg3.a04.collections;
 
-public interface SimpleList extends Iterable {
+import sun.java2d.pipe.SpanShapeRenderer;
+
+import java.util.function.Function;
+
+public interface SimpleList<T> extends Iterable<T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T o);
 
 	/**
 	 * @return current size of the list
@@ -15,5 +19,44 @@ public interface SimpleList extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
+	default SimpleList<T> filter(SimpleFilter<T> filter) {
+		SimpleList<T> result;
+		try {
+			result = (SimpleList<T>) getClass().newInstance();
+		} catch(IllegalAccessException | InstantiationException e) {
+			e.printStackTrace();
+			result = new SimpleListImpl<>();
+		}
+		for(T o : this){
+			if(filter.include(o)){
+				result.add(o);
+			}
+		}
+		return result;
+	};
+
+
+	default void addDefault(Class<T> clazz) {
+		try {
+			this.add(clazz.newInstance());
+		} catch(InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	default <R> SimpleList<R> map(Function<T, R> transform) {
+		SimpleList<R> result;
+		try {
+			result = (SimpleList<R>) getClass().newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			result = new SimpleListImpl<>();
+		}
+
+		for(T t: this) {
+			result.add(transform.apply(t));
+		}
+
+		return result;
+	}
+
 }
